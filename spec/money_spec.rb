3b2a00e920983b41f8b2356affc6138c252273e7
shuheitakada <s-takada@giftee.co>
2020-07-02 00:49:15 +0900

    第８章　Money.dollarとMoney.francを使うように変更

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,31 +3,31 @@ require './lib/money.rb'
 
 RSpec.describe Money do
   describe '#times' do
-    let(:five_dollar) { Dollar.new(5) }
-    let(:five_franc) { Franc.new(5) }
+    let(:five_dollar) { Money.dollar(5) }
+    let(:five_franc) { Money.franc(5) }
 
     it 'X倍できる' do
-      expect(five_dollar.times(2).equals(Dollar.new(10))).to be true
-      expect(five_dollar.times(3).equals(Dollar.new(15))).to be true
-      expect(five_franc.times(2).equals(Franc.new(10))).to be true
-      expect(five_franc.times(3).equals(Franc.new(15))).to be true
+      expect(five_dollar.times(2).equals(Money.dollar(10))).to be true
+      expect(five_dollar.times(3).equals(Money.dollar(15))).to be true
+      expect(five_franc.times(2).equals(Money.franc(10))).to be true
+      expect(five_franc.times(3).equals(Money.franc(15))).to be true
     end
   end
 
   describe '#equals' do
     it '等価であればtrueを返す' do
-      expect(Dollar.new(5).equals(Dollar.new(5))).to be true
-      expect(Franc.new(5).equals(Franc.new(5))).to be true
+      expect(Money.dollar(5).equals(Money.dollar(5))).to be true
+      expect(Money.franc(5).equals(Money.franc(5))).to be true
     end
 
     it '等価でなければfalseを返す' do
-      expect(Dollar.new(5).equals(Dollar.new(6))).to be false
-      expect(Franc.new(5).equals(Franc.new(6))).to be false
+      expect(Money.dollar(5).equals(Money.dollar(6))).to be false
+      expect(Money.franc(5).equals(Money.franc(6))).to be false
     end
 
     context '違う通貨と比較するとき' do
       it '5ドルと5フランは等価でない' do
-        expect(Dollar.new(5).equals(Franc.new(5))).to be false
+        expect(Money.dollar(5).equals(Money.franc(5))).to be false
       end
     end
   end
