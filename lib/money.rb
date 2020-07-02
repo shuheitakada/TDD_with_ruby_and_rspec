a0eb4d8de6833e9e9d32cf8d273a4c38e98ae5fd
shuheitakada <s-takada@giftee.co>
2020-07-03 00:53:36 +0900

    第９章　initializeメソッドをMoneyクラスに寄せる

--- lib/money.rb
+++ lib/money.rb
@@ -3,6 +3,11 @@ class Money
   attr_reader :currency
   protected :amount
 
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
+  end
+
   class << self
     def dollar(amount)
       Dollar.new(amount, 'USD')
@@ -19,22 +24,12 @@ class Money
 end
 
 class Dollar < Money
-  def initialize(amount, currency)
-    @amount = amount
-    @currency = currency
-  end
-
   def times(multiplier)
     Money.dollar(amount * multiplier)
   end
 end
 
 class Franc < Money
-  def initialize(amount, currency)
-    @amount = amount
-    @currency = currency
-  end
-
   def times(multiplier)
     Money.franc(amount * multiplier)
   end
