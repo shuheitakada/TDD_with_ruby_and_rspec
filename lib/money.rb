cee109b8e8932aac2c3ae68de369e778ec0d9722
shuheitakada <s-takada@giftee.co>
2020-07-05 03:00:43 +0900

    第１３章　Sum#reduceメソッドを作成

--- lib/money.rb
+++ lib/money.rb
@@ -38,11 +38,15 @@ class Sum
     @augend = augend
     @addend = addend
   end
+
+  def reduce(to)
+    amount = augend.amount + addend.amount
+    Money.new(amount, to)
+  end
 end
 
 class Bank
   def reduce(source, to)
-    amount = source.augend.amount + source.addend.amount
-    Money.new(amount, to)
+    source.reduce(to)
   end
 end
